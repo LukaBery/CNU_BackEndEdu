f867706d9638a5df7f41b58f3240a9b53a9bdaf4
LukaBery <dlgksqls7218@naver.com>
2023-01-02 13:45:00 +0900

    [Day1] 파일 삭제

--- Java1_2.java
+++
@@ -1,7 +0,0 @@
-public class Java1_2 {
-    public static void main(String[] args) {
-        // 타입은 int, 변수명은 value로 선언하고, 값은 12로 대입해보세요.
-        int value = 12;
-        System.out.println(value);
-    }
-}
--- Java1_4.java
+++
@@ -1,8 +0,0 @@
-public class Java1_4 {
-    public static void main(String[] args) {
-        // MAX_VALUE 이름으로 long 타입의 상수를 만들어보세요.
-        final long MAX_VALUE = 123456789999L;
-
-        System.out.println(MAX_VALUE);
-    }
-}
