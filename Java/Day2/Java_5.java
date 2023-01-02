df6285946160991839bfb2647265bb3952f76da0
LukaBery <dlgksqls7218@naver.com>
2023-01-02 16:57:18 +0900

    [Day2] 클래스와 객체, 배열
    실습 내용 추가
    
    this closes #22

---
+++ Java/Day2/Java_5.java
@@ -0,0 +1,25 @@
+package Java.Day2;
+
+public class Java_5 {
+    // [자동차 현재 속도] 필드를 선언하고 정수값 10을 저장합니다.
+    int speed = 10;
+
+    // [운행 가능 여부] 필드를 선언하고 boolean값 true를 저장합니다.
+    boolean operable= true;
+
+    // [자동차 등급] 필드를 선언하고 char값 'A'를 저장합니다.
+    char grade = 'A';
+
+    public static void main(String[] args) {
+        Java_5 car = new Java_5();
+
+        // 빈 칸에 car인스턴스의 [자동차 현재 속도] 필드를 호출합니다.
+        System.out.println(car.speed);
+
+        // 빈 칸에 car인스턴스의 [운행 가능 여부] 필드를 호출합니다.
+        System.out.println(car.operable);
+
+        // 빈 칸에 car인스턴스의 [자동차 등급] 필드를 호출합니다.
+        System.out.println(car.grade);
+    }
+}
