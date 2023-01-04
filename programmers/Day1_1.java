b1e871c386ebb9e1da9dd18f76ab35c228ceddd1
LukaBery <dlgksqls7218@naver.com>
2023-01-04 15:45:30 +0900

    [programmers study] day 1
    프로그래머스 수업 1일차
    
    this closes #54

---
+++ programmers/Day1_1.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class Day1_1 {
+    public static  void main(String[] arge){
+        int score = 87;
+
+        if(score >= 90){
+            System.out.println("A");
+        } else if (score >= 85) {
+            System.out.println("B+");
+        } else {
+            System.out.println("B");
+
+        }
+    }
+    // 알 쓸 송 잡
+}
