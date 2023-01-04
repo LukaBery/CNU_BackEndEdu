eb02ff50cdd5ea617fc4698fb0429d25719ac4f6
LukaBery <dlgksqls7218@naver.com>
2023-01-04 14:11:15 +0900

    [OnedayCoding_3] [하] 성심당 작은 메아리
    코딩 테스트 실습
    
    this closes #52

---
+++ CodingTest/OneDay/Bread.java
@@ -0,0 +1,21 @@
+package CodingTest.OneDay;
+
+import java.util.Scanner;
+
+public class Bread {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String br1 = sc.nextLine();
+        int price1 = Integer.parseInt(sc.nextLine());
+        String br2 = sc.nextLine();
+        int price2 = Integer.parseInt(sc.nextLine());
+        if(price1> price2) System.out.println(br1);
+        else if (price2> price1) System.out.println(br2);
+        else if (price1 == price2) {
+            System.out.println("금액이 같습니다.");
+
+        }
+    }
+
+}
